c4d45f4b0729a2988dec1eab1f1588186c1c8bc9
Your Name <you@example.com>
2022-01-09 00:01:13 +0600

    server first updated

---
+++ index.js
@@ -0,0 +1,148 @@
+
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+const  admin = require("firebase-admin");
+const { MongoClient } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+const  serviceAccount = require('./doctorportal-firebase-sdk.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yekno.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function verifyToken (req, res, next) {
+  if(req.headers?.authorization?.startsWith('Bearer ')){
+      const token = req.headers.authorization.split(' ')[1];
+
+      try{
+          const decodedUser = await admin.auth().verifyIdToken(token);
+          req.decodedEmail = decodedUser.email;
+
+      }
+      catch{
+
+      }
+  }
+    next();
+
+}
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('doctors_portal');
+        const appointmentsCollection = database.collection('appointments');
+        const usersCollection = database.collection('users');
+
+
+        app.get('/appointments', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+
+            const query = { email: email, date: date }
+
+            const cursor = appointmentsCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+
+
+ app.get('/users/:email', async (req, res) => {
+     const email = req.params.email;
+     const query = { email: email};
+     const user = await usersCollection.findOne(query);
+     let isAdmin = false;
+     if(user?.role === 'admin') {
+        isAdmin = true;
+     }
+res.json({admin: isAdmin})
+
+ })
+
+
+            
+// booking details insert  database
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            res.json(result)
+        });
+
+        app.post('/users', async(req, res) => {
+            const user = req.body; 
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        
+        });
+
+  app.put('/users', async(req, res) => {
+    const user = req.body;
+    const filter = { email: user.email };
+    const options = { upsert: true };
+    const updateDoc = { $set: user };
+    const result = await usersCollection.updateOne(filter, updateDoc, options);
+    res.json(result);
+
+  })
+   
+   app.put('/users/admin', verifyToken, async (req, res) => {
+       const user = req.body;
+       const requster = req.decodedEmail;
+      if(requster){
+  const  requsterAccount = await usersCollection.findOne({email: 
+    requster});
+  if(requsterAccount.role === 'admin'){
+
+    const filter =  { email: user.email };
+       const updateDoc = { $set: {role: 'admin' } };
+       const result = await usersCollection.updateOne(filter, updateDoc)
+       res.json(result)
+      }
+      }
+   else{
+       res.status(403).json({ message: 'you do not have Aceess to this page Thanks'})
+   }
+
+   })
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello Doctors portal!')
+})
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`)
+})
+
+
+//all comment details
+// app.get('/users')
+// app.post('/users')
+// app.get('/users/:id')
+// app.put('/users/:id');
+// app.delete('/users/:id')
+// users: get
+// users: post
